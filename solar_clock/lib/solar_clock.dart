f34ce23bf392db61d92315967933ea75dc7463dd
PolarisChen <polaris_chen@163.com>
2020-01-18 19:54:01 -0800

    Use correct rendered size

--- solar_clock/lib/solar_clock.dart
+++ solar_clock/lib/solar_clock.dart
@@ -6,6 +6,7 @@ import 'package:flutter/semantics.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:intl/intl.dart';
 import 'package:vector_math/vector_math_64.dart' show radians;
+import 'package:after_layout/after_layout.dart';
 
 import 'drawn_star.dart';
 
@@ -18,11 +19,11 @@ class SolarClock extends StatefulWidget {
   _SolarClockState createState() => _SolarClockState();
 }
 
-class _SolarClockState extends State<SolarClock> {
+class _SolarClockState extends State<SolarClock> with AfterLayoutMixin<SolarClock> {
   var _now = DateTime.now();
   var _condition = '';
   Timer _timer;
-  GlobalKey _key = GlobalKey();
+  var _size;
 
   @override
   void initState() {
@@ -68,62 +69,75 @@ class _SolarClockState extends State<SolarClock> {
 
   @override
   Widget build(BuildContext context) {
-    final time = DateFormat.Hms().format(DateTime.now());
+    // print('build $_hasFirstLayout');
+    if (_size != null) {
+      final time = DateFormat.Hms().format(DateTime.now());
 
-    final backgroundColor = Color(0xFFFFFFFF);
-    final sunColor = Color(0xFFD2E3FC);
-    final earthColor = Color(0xFF1113FC);
-    final moonColor = Color(0xF201030C);
- 
-    final size = MediaQuery.of(super.context).size;
+      final backgroundColor = Color(0xFFFFFFFF);
+      final sunColor = Color(0xFFD2E3FC);
+      final earthColor = Color(0xFF1113FC);
+      final moonColor = Color(0xF201030C);
+  
+      final anchorRadius = 50.0;
+      final anchorCenter = _size.center(Offset.zero);
 
-    final anchorRadius = 50.0;
-    final anchorCenter = size.center(Offset.zero);
+      final hourRadius = 20.0;
+      final hourDistance = _size.height / 2.0 - hourRadius - 30.0;
+      final hourRadian = _now.hour * radians(360 / 12) + _now.minute * radians(360 / 12 / 60) + _now.second * radians(360 / 12 / 60 / 60) - pi / 2.0;
+      final hourCenter = anchorCenter + Offset.fromDirection(hourRadian, hourDistance);
 
-    final hourRadius = 20.0;
-    final hourDistance = size.height / 2.0 - hourRadius - 30.0;
-    final hourRadian = _now.hour * radians(360 / 12) + _now.minute * radians(360 / 12 / 60) + _now.second * radians(360 / 12 / 60 / 60) - pi / 2.0;
-    final hourCenter = anchorCenter + Offset.fromDirection(hourRadian, hourDistance);
+      final minuteRadius = 10.0;
+      final minuteDistance = hourRadius + 10.0 + minuteRadius;
+      final minuteRadian = _now.minute * radians(360 / 60) + _now.second * radians(360 / 60 / 60) - pi / 2.0;
+      final minuteCenter = hourCenter + Offset.fromDirection(minuteRadian, minuteDistance);
 
-    final minuteRadius = 10.0;
-    final minuteDistance = hourRadius + 10.0 + minuteRadius;
-    final minuteRadian = _now.minute * radians(360 / 60) + _now.second * radians(360 / 60 / 60) - pi / 2.0;
-    final minuteCenter = hourCenter + Offset.fromDirection(minuteRadian, minuteDistance);
+      final sun = DrawnStar(
+        color: sunColor,
+        radius: anchorRadius,
+        center: anchorCenter,
+      );
 
-    final sun = DrawnStar(
-      color: sunColor,
-      radius: anchorRadius,
-      center: anchorCenter,
-    );
+      final earth = DrawnStar(
+        color: earthColor,
+        radius: hourRadius,
+        center: hourCenter,
+      );
 
-    final earth = DrawnStar(
-      color: earthColor,
-      radius: hourRadius,
-      center: hourCenter,
-    );
-
-    final moon = DrawnStar(
-      color: moonColor,
-      radius: minuteRadius,
-      center: minuteCenter,
-    );
-    
-    return Semantics.fromProperties(
-      properties: SemanticsProperties(
-        label: 'Solar clock with time $time',
-        value: time,
-      ),
-      child: Container(
-        color: backgroundColor,
-        child: Stack(
-          children: <Widget>[
-            Text('$time $_condition'),
-            sun,
-            earth,
-            moon,
-          ],
+      final moon = DrawnStar(
+        color: moonColor,
+        radius: minuteRadius,
+        center: minuteCenter,
+      );
+      
+      return Semantics.fromProperties(
+        properties: SemanticsProperties(
+          label: 'Solar clock with time $time',
+          value: time,
         ),
-      ),
-    );
+        child: Container(
+          color: backgroundColor,
+          child: Stack(
+            children: <Widget>[
+              Text('$time $_condition'),
+              sun,
+              earth,
+              moon,
+            ],
+          ),
+        ),
+      );
+    }
+    return Container();
   }
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    setState(() {
+      final RenderBox renderBox = context.findRenderObject();
+      _size = renderBox.size;
+    });
+  }
+
+  // TODO: Handle afterRotateCase
+  // TODO: Handle screen size to star radius
 }
